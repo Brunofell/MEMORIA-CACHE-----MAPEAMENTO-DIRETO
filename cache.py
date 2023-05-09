00355145669946e2c0128c230c18e4e390685fdb
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-05-09 16:03:44 -0300

    continuando...

--- cache.py
+++ cache.py
@@ -1,8 +1,6 @@
 # Tópicos 1 e 2:
 
 def inicializaCache(tam_cache):
-    posicaoM = []
-    posicaoC = []
     c = 0
     for cont in range(tam_cache):
         posicaoM.append(-1)
@@ -19,26 +17,49 @@ def inicializaCache(tam_cache):
 # Tópico 3
 
 def imprimirCache():
-    print("TAMANHO DA CACHE: ", tam_cache)
-    inicializaCache(tam_cache)
-
+    print("-" * 50)
+    print("TAMANHO DA CACHE: ")
+    for c in posicaoC:
+        print(c)
+    print("-" * 50)
+    print("TAMANHO DA MEMÓRIA: ")
+    for c in posicaoM:
+        print(c)
 
 # Tópico 4
 
-def mapeamento_direto(tam_cache, pos_memoria):
+def mapeamento_direto(pos_memoria):
+    contador = 0
+    miss = 0
+    hit = 0
     posicao = int(input("QUANTAS POSIÇÃO DA MEMÓRIA VOCÊ DESEJA ACESSAR: "))
     for c in range(posicao):
         valor = int(input("- QUAL POSIÇÃO NA MEMÓRIA VOCÊ DESEJA ACESSAR: "))
         pos_memoria.append(valor)
-    print(pos_memoria)
-    print(tam_cache)
+    for item in pos_memoria:
+        if item in posicaoM:
+            print(f"{item}: HIT")
+            hit += 1
+        else:
+            print(f"{item}: MISS")
+            miss += 1
+            posicaoM[contador] = item
+            contador += 1
+
+    taxa_acertos = (hit/posicao) * 100
+    imprimirCache()
+    print("-" * 50)
+    print("- MEMÓRIAS ACESSADAS: ", posicao)
+    print("- Número de HITS: ", hit)
+    print("- Número de MISS: ", miss)
+    print("- TAXA DE ACERTOS: %.2f%%" % taxa_acertos)
+
 
 
 tam_cache = int(input("- Digite o tamanho da memória cache: "))
-
-imprimirCache()
-
 pos_memoria = []
+posicaoM = []
+posicaoC = []
+inicializaCache(tam_cache)
 
-mapeamento_direto(tam_cache, pos_memoria)
-
+mapeamento_direto(pos_memoria)
